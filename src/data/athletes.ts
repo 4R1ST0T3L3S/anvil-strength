3927b3d5dbb1a8b63e43d044c9e35186f9612dcc
Javier Bou Martinez <jotabou@protonmail.com>
2026-01-27 13:27:53 +0100

    fix: update marc and pau image paths

--- src/data/athletes.ts
+++ src/data/athletes.ts
@@ -22,12 +22,12 @@ export const athletes: Athlete[] = [
     id: 3,
     name: "Marc Alonso",
     category: "-105kg JNR",
-    image: "https://images.unsplash.com/photo-1599058945522-28d584b6f0ff?q=80&w=1000&auto=format&fit=crop"
+    image: "/athletes/marc.png"
   },
   {
     id: 4,
     name: "Pau",
     category: "-105kg SBJ",
-    image: "https://images.unsplash.com/photo-1581009146145-b5ef050c2e1e?q=80&w=1000&auto=format&fit=crop"
+    image: "/athletes/Pau.png"
   }
 ];
