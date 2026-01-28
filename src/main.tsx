21a2d8174faf91f2dd16f130ab9ab1ca745c4580
4R1ST0T3L3S <jotabou@protonmail.com>
2026-01-28 19:26:44 +0100

    fix(pwa): explicitly register service worker in main.tsx for firefox compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  onNeedRefresh() { },
+  onOfflineReady() {
+    console.log('App ready to work offline')
+  },
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
