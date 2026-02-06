2c8e51200281d9afc82743d194d376dd66aa5a62
Maralonsoo <marcalonsopascual@gmail.com>
2026-02-06 15:00:41 +0100

    foto bou arreglada

--- src/data/coaches.ts
+++ src/data/coaches.ts
@@ -25,7 +25,7 @@ export const coaches: Coach[] = [
     id: 2,
     name: "Javier Bou",
     role: "ENTRENADOR DE POWERLIFTING",
-    image: "/javier-bou.jpg", 
+    image: "/javier-bou.jpeg", 
     instagram: "https://www.instagram.com/boustrength?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
     contactForm: "https://docs.google.com/forms/d/e/1FAIpQLSfACXcjom-FbRi1a-PNyrJdvY_wAO9Hpa3_hktaNY82QDx4tg/viewform",
     bio: "Apasionado del Powerlifting y el entrenamiento de fuerza. Con años de experiencia en competición y preparación de atletas, mi objetivo es perfeccionar la técnica y la programación para alcanzar números de élite. La disciplina y la constancia son los pilares de mi metodología."
