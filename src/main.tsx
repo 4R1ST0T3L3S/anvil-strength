921ee35d5c4afcf48053fae386b02a4ef9df2d14
Javier Bou Martinez <jotabou@protonmail.com>
2026-02-19 13:49:40 +0100

    feat: add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import './index.css'
 
@@ -43,6 +44,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <AuthProvider>
         <BrowserRouter>
           <App />
+          <Analytics />
         </BrowserRouter>
       </AuthProvider>
     </QueryClientProvider>
